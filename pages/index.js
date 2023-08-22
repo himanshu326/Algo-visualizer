56b960832b1a58cfb86c5050d3dfea1bf11f3380
himanshu326 <87376624+himanshu326@users.noreply.github.com>
2023-08-22 22:46:34 +0530

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,64 @@
+import { useEffect } from "react";
+import { NextSeo } from "next-seo";
+
+import TopBar from "/components/TopBar";
+import Terrain from "/components/Home/Terrain";
+import HeroTitle from "/components/Home/heroTitle";
+import { batch, useDispatch } from "react-redux";
+import {
+  setAlgoId,
+  setAlgoName,
+  setAlgoCategory,
+} from "/redux/reducers/pageSlice";
+
+export default function Home() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    batch(() => {
+      dispatch(setAlgoId("home"));
+      dispatch(setAlgoName("Home"));
+      dispatch(setAlgoCategory("home"));
+    });
+  });
+  return (
+    <div>
+      <NextSeo
+        title="Home | Algospace"
+        description="A algorithm visualizer for understanding algorithms"
+        canonical={`https://algospace.vercel.app/`}
+        openGraph={{
+          url: `https://algospace.vercel.app/`,
+          title: "Home | Algospace",
+          description: "A algorithm visualizer for understanding algorithms",
+          profile: {
+            firstName: "Himanshu",
+            lastName: "Yadav",
+          },
+          images: [
+            {
+              url: "https://algospace.vercel.app/social.png",
+              width: 800,
+              height: 600,
+              alt: "Og Image Alt",
+              type: "image/jpeg",
+            },
+          ],
+          siteName: "Algospace",
+        }}
+      />
+      <div className="p-gap">
+        <div className="relative w-[100%] h-[70vh] border-[1px] border-border-1 overflow-hidden">
+          <TopBar />
+          <div
+            id="visualizer-container"
+            className="relative h-full overflow-hidden bg-hero bg-cover bg-center bg-no-repeat"
+          >
+            <HeroTitle />
+            <Terrain className="absolute top-[-100%] z-10" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
